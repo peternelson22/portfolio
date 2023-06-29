05abb2c6ff54a33a86e157a28eda89fc3f1e3d82
nelson <peternelson063@gmail.com>
2023-06-29 23:50:29 +0100

    Added react-typewriter

---
+++ src/components/BackgroundCircles.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+
+type Props = {};
+const BackgroundCircles = (props: Props) => {
+  return (
+    <div>
+      <div className=''>
+        <div className='' />
+        <div className='' />
+        <div className='' />
+        <div className='' />
+        <div className='' />
+      </div>
+    </div>
+  );
+};
+export default BackgroundCircles;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,28 @@
-type Props = {}
+'use client';
+
+//import { Typewriter } from 'react-simple-typewriter';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {};
 const Hero = (props: Props) => {
+  const [text] = useTypewriter({
+    words: [
+      "Hello, I'm Nelson Tanko",
+      'A Super Engineer',
+      'BACKEND, FRONTEND, EVERYWHERE!!!',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
-    <div>Hero</div>
-  )
-}
-export default Hero
+    <div>
+      <BackgroundCircles/>
+      <h2>
+        <span>{text}</span>
+        <Cursor cursorColor='teal' />
+      </h2>
+    </div>
+  );
+};
+export default Hero;
